c19ed56596e2d60858229e0d589a104d011c8cbd
Thanh Bình <nguyenthanhbinhntb9@gmail.com>
2022-10-29 09:39:20 +0700

    Kết nối sql
    Kết nối sql

--- QuanLyKTX/QuanLyKTX/Data Provider/DataProvider.cs
+++ QuanLyKTX/QuanLyKTX/Data Provider/DataProvider.cs
@@ -1,12 +1,143 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace QuanLyKTX.Data_Provider
 {
-    class DataProvider
+    public class DBConnection
     {
+        private SqlDataAdapter adapter;
+        private SqlConnection connection;
+        public DBConnection()
+        {
+            adapter = new SqlDataAdapter();
+            connection = new SqlConnection(Properties.Settings.Default.s);
+        }
+        private SqlConnection openConnection()
+        {
+            if (connection.State == ConnectionState.Closed || connection.State ==
+            ConnectionState.Broken)
+            {
+                connection.Open();
+            }
+            return connection;
+        }
+
+        public void executeInsertQuery(String query, SqlParameter[] sqlParameter)
+        {
+            using (SqlCommand sqlCommand = new SqlCommand(query, openConnection()))
+            {
+                sqlCommand.CommandType = CommandType.Text;
+                sqlCommand.Parameters.AddRange(sqlParameter);
+                try
+                {
+                    sqlCommand.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        public object executeScalar(String query, SqlParameter[] sqlParameter)
+        {
+            using (SqlCommand sqlCommand = new SqlCommand(query, openConnection()))
+            {
+                sqlCommand.CommandType = CommandType.Text;
+                sqlCommand.Parameters.AddRange(sqlParameter);
+                object result = new object();
+                try
+                {
+                    result = sqlCommand.ExecuteScalar();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+                return result;
+            }
+        }
+        public DataTable executeReader(String query, SqlParameter[] sqlParameter)
+        {
+            using (SqlCommand sqlCommand = new SqlCommand(query, openConnection()))
+            {
+                sqlCommand.CommandType = CommandType.Text;
+                sqlCommand.Parameters.AddRange(sqlParameter);
+                DataTable dt = new DataTable();
+                try
+                {
+                    SqlDataReader dataReader = sqlCommand.ExecuteReader(CommandBehavior.CloseConnection);
+                    dt.Load(dataReader);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+                return dt;
+            }
+        }
+        public int executeCount(string query)           // trả về số lượng 
+        {
+            int cout = 0;
+            using (SqlCommand sqlCommand = new SqlCommand(query, openConnection()))
+            {
+                try
+                {
+                    cout = (Int32)sqlCommand.ExecuteScalar();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+                return cout;
+            }
+        }
+
+        public DataTable executeLoadData(string query)      // trả về bảng dữ liệu
+        {
+            using (SqlCommand command = new SqlCommand(query, openConnection()))
+            {
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    DataTable dt = new DataTable();
+                    try
+                    {
+                        dt.Load(reader);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                    finally
+                    {
+                        connection.Close();
+                        reader.Close();
+                    }
+                    return dt;
+                }
+            }
+        }
     }
 }
