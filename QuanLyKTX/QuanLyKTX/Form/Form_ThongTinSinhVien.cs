784dbe7851551966d1963104fecc6b50d7eabe66
trungdq2706 <trungdq.de@gmail.com>
2022-10-29 10:19:12 +0700

    FrmSinhVien
    Thiết kế form-SinhVien

---
+++ QuanLyKTX/QuanLyKTX/Form/Form_ThongTinSinhVien.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyKTX
+{
+    public partial class Form_ThongTinSinhVien : Form
+    {
+        public Form_ThongTinSinhVien()
+        {
+            InitializeComponent();
+        }
+    }
+}
